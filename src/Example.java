323aade338aeb4d67290997fb97961f18e6f1a9f
fin-ger <christensen.fin@gmail.com>
2013-10-16 07:11:49 -0700

    Eclipse changed formatting

--- src/Example.java
+++ src/Example.java
@@ -1,15 +1,15 @@
 import github.fin_ger.term.*;
 
 @Termconf (usage   = "java Example [option] [arguments]",
-	   example = "java Example -t ha 5")
+           example = "java Example -t ha 5")
 public class Example {
     public static void main (String[] args) {
 	Term.init ("Example", args);
     }
 
     @Termmeth (short_param = "t",
-	       description = "This is a test method",
-	       arguments   = { "TEXT", "REPETITIONS" })
+	           description = "This is a test method",
+	           arguments   = { "TEXT", "REPETITIONS" })
     public static void test (String a, int b) {
 	String res = "";
 	for (int i = 0; i < b; i++) {
@@ -19,16 +19,16 @@ public class Example {
     }
 
     @Termmeth (short_param = "c",
-	       description = "Concatenate two strings",
-	       arguments   = { "TEXT", "TEXT" })
+	           description = "Concatenate two strings",
+	           arguments   = { "TEXT", "TEXT" })
     public static String cat (String a, String b) {
 	return a + b;
     }
 
     @Termmeth (short_param = "m",
-	       description = "Multiplicate two integer values",
-	       des_form    = false,
-	       arguments   = { "INT", "INT" })
+	           description = "Multiplicate two integer values",
+	           des_form    = false,
+	           arguments   = { "INT", "INT" })
     public static int mul (int a, int b) {
 	return a * b;
     }
